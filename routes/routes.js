8035ba5a9d549c076c316e5b5969df4425a7c409
Sammydepoj <samueladepoju86@yahoo.com>
2023-08-20 07:49:13 +0100

    routing fixes

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ const { signin } = require("../controllers/signin.controllers");
 const signUp = require("../controllers/signupController");
 const auth = require("../middleware/auth");
 const ClockController = require("../controllers/clocks.controller");
+const { UsersController } = require("../controllers/user.controller");
 
 const Router = express.Router();
 
@@ -11,5 +12,7 @@ Router.post("/signup", signUp);
 Router.post("/clockin", auth, ClockController.clockIn);
 Router.post("/clockout", auth, ClockController.clockOut);
 Router.get("/getAllClockIn", auth, ClockController.clockInHistory);
+Router.get("/getAllParticipants", auth, UsersController.getAllParticipants);
+Router.get("/getUserInfo", auth, UsersController.getUserInfo);
 
 module.exports = Router;
